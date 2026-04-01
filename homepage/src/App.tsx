c812b403a2464fed937a543eba5ffe0ec592e863
Yazeedj <glzyww22@gmail.com>
2026-04-01 16:21:54 -0400

    Update homepage with explore-page changes
    Brings in Explore page, About, Contact pages, Carousel component,
    and updated Navbar from the explore-page branch into homepage/.

--- homepage/src/App.tsx
+++ homepage/src/App.tsx
@@ -1,12 +1,20 @@
-import Header from "./components/Header.tsx"
-import Navbar from "./components/Navbar.tsx"
-import "@fontsource/michroma"
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home.tsx'
+import Explore from './pages/Explore.tsx'
+import About from './pages/About.tsx'
+import Contact from './pages/Contact.tsx'
+import Navbar from './components/Navbar.tsx'
 
 function App() {
   return (
     <>
       <Navbar />
-      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/explore' element={<Explore />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
     </>
   )
 }
---
+++ homepage/src/pages/Explore.tsx
@@ -0,0 +1,23 @@
+import Carousel from "../components/Carousel.tsx"
+import "../App.css"
+
+function Explore() {
+  return (
+    <>
+      <div className="page">
+        <h1 className="page-header">Most Downloaded Presets of the Month</h1>
+        <Carousel />
+        <h1 className="page-header">Most Downloaded Presets for Dubstep</h1>
+        <Carousel />
+        <h1 className="page-header">Most Downloaded Presets for Synthwave</h1>
+        <Carousel />
+        <h1 className="page-header">Most Downloaded Presets for Pop</h1>
+        <Carousel />
+        <h1 className="page-header">Most Dowloaded Presets for Hip Hop</h1>
+        <Carousel />
+      </div>
+    </>
+  )
+}
+
+export default Explore
